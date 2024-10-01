33ad414cb2d149cf1588e4b8f7541572526ed705
Santosh Kumawat <santoshkumaawat@gmail.com>
2024-10-02 00:59:13 +0530

    fix typo in journals

--- src/main/java/com/santoshkumawat/journalApp/entity/JournalEntry.java
+++ src/main/java/com/santoshkumawat/journalApp/entity/JournalEntry.java
@@ -1,10 +1,9 @@
 package com.santoshkumawat.journalApp.entity;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -13,10 +12,12 @@ import java.time.LocalDateTime;
 //@Getter
 @Data
 @Document(collection = "journalEntry")
+@NoArgsConstructor
 public class JournalEntry {
 
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime created;
