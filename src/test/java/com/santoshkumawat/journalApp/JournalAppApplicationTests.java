f65b5448eebcd62a54b6e118db199cf3eb8094c7
Santosh Kumawat <santoshkumaawat@gmail.com>
2025-01-17 15:52:37 +0530

    added JavaMailSender using smtp

--- src/test/java/com/santoshkumawat/journalApp/JournalAppApplicationTests.java
+++ src/test/java/com/santoshkumawat/journalApp/JournalAppApplicationTests.java
@@ -1,6 +1,8 @@
 package com.santoshkumawat.journalApp;
 
 import com.santoshkumawat.journalApp.repository.UserRepositoryImpl;
+import com.santoshkumawat.journalApp.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -8,15 +10,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class JournalAppApplicationTests {
 
 	@Autowired
 	private UserRepositoryImpl userRepository;
 
+	@Autowired
+	private EmailService emailService;
+
 	@Disabled("tested")
 	@Test
 	void testSaveNewUser() {
 		Assertions.assertNotNull(userRepository.getUsersForSA());
+	}
+
+	@Test
+	public  void testSendMail(){
+		log.info("Sending mail =====================================================================");
+		emailService.sendMail("santoshkumawat.sde@gmail.com", "Test Mail", "How are you!");
 	}
 
 }
